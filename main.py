cc609cf3f5d12ba25d492085d70e71290f690268
ShaahinChaichi <shaahin1974@gmail.com>
2023-05-04 15:10:20 +0330

    play function tested and is ok

--- main.py
+++ main.py
@@ -5,27 +5,31 @@ random_number = random.randrange(1, 100)
 
 def play(level):
     print(random_number)
-
     if level == "easy":
+        for play_number in range(9):
+            guess_number = int(input("Please guess a number: "))
+            if guess_number > random_number:
+                print("Too high, guess again")
+                print(f"Number of guess {9 - play_number}")
+                play_number += 1
+            elif guess_number < random_number:
+                print("Too low, guess again")
+                print(f"Number of guess {9 - play_number}")
+                play_number += 1
+            else:
+                print("Success")
+
+    if level == "hard":
         for play_number in range(5):
             guess_number = int(input("Please guess a number: "))
             if guess_number > random_number:
                 print("Too high, guess again")
-                play("easy")
+                print(f"Number of guess {5 - play_number}")
+                play_number += 1
             elif guess_number < random_number:
                 print("Too low, guess again")
-                play("easy")
-            else:
-                print("Success")
-    elif level == "hard":
-        for play_number in range(10):
-            guess_number = int(input("Please guess a number: "))
-            if guess_number > random_number:
-                print("Too high, guess again")
-                play("easy")
-            elif guess_number < random_number:
-                print("Too low, guess again")
-                play("easy")
+                print(f"Number of guess {5 - play_number}")
+                play_number += 1
             else:
                 print("Success")
 
