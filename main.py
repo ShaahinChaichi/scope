f4550525040bb2e9ebb030013194bfc4446e9989
ShaahinChaichi <shaahin1974@gmail.com>
2023-05-04 15:28:39 +0330

    start_of_project function added

--- main.py
+++ main.py
@@ -1,39 +1,52 @@
 import random
 
-random_number = random.randrange(1, 100)
-
 
 def play(level):
+    random_number = random.randrange(1, 100)
     print(random_number)
+    is_game_over = False
     if level == "easy":
-        for play_number in range(9):
-            guess_number = int(input("Please guess a number: "))
-            if guess_number > random_number:
-                print("Too high, guess again")
-                print(f"Number of guess {9 - play_number}")
-                play_number += 1
-            elif guess_number < random_number:
-                print("Too low, guess again")
-                print(f"Number of guess {9 - play_number}")
-                play_number += 1
-            else:
-                print("Success")
+        while not is_game_over:
+            for play_number in range(9):
+                guess_number = int(input("Please guess a number: "))
+                if guess_number > random_number:
+                    print("Too high, guess again")
+                    print(f"Number of guess {9 - play_number}")
+                    play_number += 1
+                elif guess_number < random_number:
+                    print("Too low, guess again")
+                    print(f"Number of guess {9 - play_number}")
+                    play_number += 1
+                else:
+                    print("Success")
+                    start_of_project()
 
-    if level == "hard":
-        for play_number in range(5):
-            guess_number = int(input("Please guess a number: "))
-            if guess_number > random_number:
-                print("Too high, guess again")
-                print(f"Number of guess {5 - play_number}")
-                play_number += 1
-            elif guess_number < random_number:
-                print("Too low, guess again")
-                print(f"Number of guess {5 - play_number}")
-                play_number += 1
-            else:
-                print("Success")
+    while not is_game_over:
+        if level == "hard":
+            for play_number in range(5):
+                guess_number = int(input("Please guess a number: "))
+                if guess_number > random_number:
+                    print("Too high, guess again")
+                    print(f"Number of guess {5 - play_number}")
+                    play_number += 1
+                elif guess_number < random_number:
+                    print("Too low, guess again")
+                    print(f"Number of guess {5 - play_number}")
+                    play_number += 1
+                else:
+                    print("Success")
+                    is_game_over = True
+                    start_of_project()
 
 
-easy_or_hard = input("'easy' or 'hard' ?")
+def start_of_project():
+    continue_game = input("Do you want to continue? ('y' or 'n') ")
+    if continue_game == 'y':
+        easy_or_hard = input("'easy' or 'hard' ?")
+        play(easy_or_hard)
+    else:
+        print("Thanks for playing !!!!")
+        return
 
-play(easy_or_hard)
+
+start_of_project()
